cf05435d675092e33cbe3ea1504add4f56872ffc
beizighamza <hamza.beizg@neofacto.com>
2022-11-16 23:04:24 +0100

    hamza 2

--- tuniflix/src/main/java/com/example/android/tuniflix/data/MovieRepository.java
+++ tuniflix/src/main/java/com/example/android/tuniflix/data/MovieRepository.java
@@ -1,18 +1,3 @@
-/*
- *  Copyright 2018 Soojeong Shin
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 
 package com.example.android.tuniflix.data;
 
@@ -38,7 +23,7 @@ import static com.example.android.tuniflix.utilities.Constant.LANGUAGE;
 import static com.example.android.tuniflix.utilities.Constant.PAGE;
 
 /**
- * MovieRepository is responsible for handling data operations in PopularMovies. Acts as a mediator
+ * MovieRepository is responsible for handling data operations in Tuniflix. Acts as a mediator
  * between {@link TheMovieApi} and {@link MovieDao}
  */
 public class MovieRepository {

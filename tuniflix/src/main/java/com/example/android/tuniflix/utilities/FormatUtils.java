fd3bde8252cecad4c73ad6200d3667558d97bea6
beizighamza <hamza.beizg@neofacto.com>
2022-11-17 03:07:13 +0100

    final

--- tuniflix/src/main/java/com/example/android/tuniflix/utilities/FormatUtils.java
+++ tuniflix/src/main/java/com/example/android/tuniflix/utilities/FormatUtils.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 import static com.example.android.tuniflix.utilities.Constant.ZERO;
 
 /**
- * Class for handling conversions that are useful for PopularMovies.
+ * Class for handling conversions that are useful for Tuniflix.
  */
 public class FormatUtils {
 
